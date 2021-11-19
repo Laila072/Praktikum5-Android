c415ef32ee7717fc9cf4dabb696105d18c96a848
Laila072 <norsadah@gmail.com>
2021-11-19 10:16:14 +0800

    Initial commit

---
+++ app/src/main/java/com/Praktikum1/praktikum/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.Praktikum1.praktikum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val simpanButton = findViewById<Button>(R.id.simpanButton)
+        val batalButton = findViewById<Button>(R.id.batalButton)
+
+        val namaEditText = findViewById<EditText>(R.id.namaEditText)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val noHandphoneEditText = findViewById<EditText>(R.id.noHandphoneEditText)
+        val tanggalLahirEditText = findViewById<EditText>(R.id.tanggalLahirEditText)
+        val jarakEditText = findViewById<EditText>(R.id.jarakEditText)
+
+
+        simpanButton.setOnClickListener {
+            val  nama = namaEditText.text.toString()
+            var jarak = jarakEditText.text.toString().toInt()
+            jarak = jarak * 1000
+            Toast.makeText(this,"Hai $nama",Toast.LENGTH_SHORT).show()
+        }
+
+        batalButton.setOnClickListener {
+            finish()
+        }
+    }
+}
